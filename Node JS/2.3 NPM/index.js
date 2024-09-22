60c0af8ab75fd3669174f3e8b28fc2754ef6e768
uw-user <kgiris2o57@gmail.com>
2024-09-22 01:37:10 -0600

    Startig Node

---
+++ Node JS/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+//var generateName = require("sillyname");
+// import generateName from "sillyname";
+
+// var sillyname = generateName();
+// console.log('My name is ')
+// console.log(sillyname)
+
+import superheros from "superheroes";
+const name = superheros.random()
+console.log(name)
